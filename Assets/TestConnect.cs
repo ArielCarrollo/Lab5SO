ae90392777e62587824a6d1f6ff1f48233bd5892
Tecsup <harvy.carrollo@tecsup.edu.pe>
2025-04-14 11:54:33 -0500

    e

---
+++ Assets/TestConnect.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PhotonConnectionTest : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        Debug.Log("Iniciando conexión a Photon...");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("? Conectado al servidor de Photon.");
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName);
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("?? Conectado al lobby de Photon.");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarning($"? Desconectado de Photon. Motivo: {cause}");
+    }
+}
